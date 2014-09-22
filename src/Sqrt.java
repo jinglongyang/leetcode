51dc079c573bc157a11a5034c98a33ec84fc9d1f
Aaron Yang <jinglongyang@real.com>
2014-09-22 11:18:16 -0700

    added leetcode oj linke

--- src/Sqrt.java
+++ src/Sqrt.java
@@ -1,23 +1,24 @@
 /**
+ * https://oj.leetcode.com/problems/sqrtx/
+ * <p/>
  * Implement int sqrt(int x).
- * Compute and return the square root of x. 
- *
- *Note: Need to decalre the i,j,mid as long in order to avoid overflow when compute square.
- *
+ * Compute and return the square root of x.
+ * <p/>
+ * Note: Need to declare the i,j,mid as long in order to avoid overflow when compute square.
  */
 public class Sqrt {
-  public int sqrt(int x) {
+    public int sqrt(int x) {
         long i = 0, j = x / 2 + 1;
         while (i <= j) {
             long mid = i + (j - i) / 2;
             long square = mid * mid;
-            if (square == x) return (int)mid;
+            if (square == x) return (int) mid;
             if (square < x) {
                 i = mid + 1;
             } else {
                 j = mid - 1;
             }
         }
-        return (int)j;
+        return (int) j;
     }
 }
