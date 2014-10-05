f139894d2aafc079c9a9d70521e9dd8c0bf93a4f
Aaron Yang <jinglongyang@real.com>
2014-10-04 22:52:36 -0700

    Permutations

---
+++ src/permutation/Permutations.java
@@ -0,0 +1,42 @@
+package permutation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://oj.leetcode.com/problems/permutations/
+ * Given a collection of numbers, return all possible permutations.
+ * For example,
+ * [1,2,3] have the following permutations:
+ * [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
+ */
+public class Permutations {
+    public List<List<Integer>> permute(int[] num) {
+        List<List<Integer>> res = new ArrayList<>();
+        int l = num == null ? 0 : num.length;
+        if (l == 0) {
+            return res;
+        }
+        helper(res, new ArrayList<Integer>(), l, num);
+        return res;
+    }
+
+    private void helper(List<List<Integer>> res, List<Integer> path, int length, int[] num) {
+        if (path.size() == length) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < length; i++) {
+            if (path.contains(num[i])) {
+                continue;
+            }
+            path.add(num[i]);
+            helper(res, path, length, num);
+            path.remove(path.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Permutations().permute(new int[]{0, 1}));
+    }
+}
