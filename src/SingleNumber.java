6afa93201009612863659c2768ba4ec4c54373cd
Aaron Yang <jinglongyang@real.com>
2014-10-09 07:56:14 -0700

    Single number

---
+++ src/SingleNumber.java
@@ -0,0 +1,15 @@
+/**
+ * https://oj.leetcode.com/problems/single-number/
+ * Given an array of integers, every element appears twice except for one. Find that single one.
+ * Note:
+ * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ */
+public class SingleNumber {
+    public int singleNumber(int[] A) {
+        int res = 0;
+        for (int a : A) {
+            res ^= a;
+        }
+        return res;
+    }
+}
