d0bbdcd7f44767cf14643d22cd17b70cb09898ea
Aaron Yang <jinglongyang@real.com>
2014-10-13 21:39:45 -0700

    added leetcode oj link

--- src/map/LRUCache.java
+++ src/map/LRUCache.java
@@ -4,7 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by jinglongyang on 10/13/14.
+ * https://oj.leetcode.com/problems/lru-cache/
+ * <p/>
+ * Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and set.
+ * <p/>
+ * get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+ * set(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity,
+ * it should invalidate the least recently used item before inserting a new item.
  */
 public class LRUCache {
     private static class Node {
