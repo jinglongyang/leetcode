c24abb9e8cda8cbe73ade80bd8880294b0b87046
Aaron Yang <jinglongyang@real.com>
2014-10-19 11:31:44 -0700

    find-minimum-in-rotated-sorted-array

---
+++ src/array/FindMinimumRotatedSortedArray.java
@@ -0,0 +1,35 @@
+package array;
+
+/**
+ * https://oj.leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+ * Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+ * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+ * Find the minimum element.
+ * <p/>
+ * You may assume no duplicate exists in the array.
+ */
+public class FindMinimumRotatedSortedArray {
+    public int findMin(int[] num) {
+        int l = num == null ? 0 : num.length;
+        if (l == 0) return 0;
+        if (l == 1) return num[0];
+        return findMin(num, 0, num.length - 1);
+    }
+
+    private int findMin(int[] num, int start, int end) {
+        if (end < start) return num[0];
+
+        if (end == start) return num[start];
+
+        int middle = start + (end - start) / 2;
+        if (middle > start && num[middle - 1] > num[middle]) {
+            return num[middle];
+        }
+        if (middle < end && num[middle + 1] < num[middle]) {
+            return num[middle + 1];
+        }
+        if (num[end] > num[middle])
+            return findMin(num, start, middle - 1);
+        return findMin(num, middle, end);
+    }
+}
