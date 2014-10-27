10b17f185797c43d662667e7e2316448d1876437
Aaron Yang <jinglongyang@real.com>
2014-10-26 21:53:13 -0700

    interleaving-string

---
+++ src/dp/InterleavingString.java
@@ -0,0 +1,37 @@
+package dp;
+
+/**
+ * https://oj.leetcode.com/problems/interleaving-string/
+ * Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
+ * For example,
+ * Given:
+ * s1 = "aabcc",
+ * s2 = "dbbca",
+ * <p/>
+ * When s3 = "aadbbcbcac", return true.
+ * When s3 = "aadbbbaccc", return false.
+ */
+public class InterleavingString {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int l1 = s1.length();
+        int l2 = s2.length();
+        int l3 = s3.length();
+        if (l3 != l1 + l2) return false;
+        boolean[][] dp = new boolean[l1 + 1][l2 + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= l1; i++) {
+            dp[i][0] = dp[i - 1][0] && s1.charAt(i - 1) == s3.charAt(i - 1);
+        }
+        for (int i = 1; i <= l2; i++) {
+            dp[0][i] = dp[0][i - 1] && s2.charAt(i - 1) == s3.charAt(i - 1);
+        }
+        for (int i = 1; i <= l1; i++) {
+            for (int j = 1; j <= l2; j++) {
+                if ((dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1))) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+        return dp[l1][l2];
+    }
+}
