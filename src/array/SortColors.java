a37f7ec579ee5749326b462dee4010ffee0d65b2
Aaron Yang <jinglongyang@real.com>
2014-11-03 21:31:30 -0800

    sort colors

---
+++ src/array/SortColors.java
@@ -0,0 +1,33 @@
+package array;
+
+/**
+ * https://oj.leetcode.com/problems/sort-colors/
+ * <p/>
+ * Given an array with n objects colored red, white or blue, sort them so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+ * Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+ */
+public class SortColors {
+    public void sortColors(int[] A) {
+        int len = A == null ? 0 : A.length;
+        if (len < 2) return;
+        int i = 0, left = 0, right = len - 1;
+        while (i < right) {
+            if (A[i] == 0) {
+                swap(A, i, left);
+                left++;
+                i++;
+            } else if (A[i] == 1) {
+                i++;
+            } else {
+                swap(A, i, right);
+                right--;
+            }
+        }
+    }
+
+    private void swap(int[] a, int i, int j) {
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+}
