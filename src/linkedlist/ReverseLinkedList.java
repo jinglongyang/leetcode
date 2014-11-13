5e931ab8fd96bb28086dc82139bd392095c26277
Aaron Yang <jinglongyang@real.com>
2014-11-12 23:11:21 -0800

    implement reverse linked list

--- src/linkedlist/ReverseLinkedList.java
+++ src/linkedlist/ReverseLinkedList.java
@@ -5,6 +5,29 @@ package linkedlist;
  * Implement the reversal of a singly linked list iteratively and recursively.
  */
 public class ReverseLinkedList {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        System.out.println(head.toString());
+
+        ReverseLinkedList reverseLinkedList = new ReverseLinkedList();
+        System.out.println(reverseLinkedList.reverse(head));
+    }
+
+    public ListNode reverse(ListNode head) {
+        if (head == null) return null;
+
+        ListNode prev = null, node = head;
+        while (node != null) {
+            ListNode next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+
+        return prev;
+    }
+
     /**
      * Definition for singly-linked list.
      */
@@ -16,9 +39,10 @@ public class ReverseLinkedList {
             val = x;
             next = null;
         }
-    }
 
-    public ListNode reverseBetween(ListNode head) {
-        return null;
+        @Override
+        public String toString() {
+            return String.valueOf(val) + (next == null ? "" : "," + next.toString());
+        }
     }
 }
